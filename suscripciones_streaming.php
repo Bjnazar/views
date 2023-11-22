56e4acd734592a87aeca63d615ccd430facaca10
Bernardita <bjnazar@uc.cl>
2023-11-22 00:01:11 -0300

    navbar

--- suscripciones_streaming.php
+++ suscripciones_streaming.php
@@ -1,7 +1,4 @@
+<?php include 'navbar.php'; ?>
 <h2>Suscripciones de películas y series</h2>
 <p>Contenido</p>
 
-<?php
-// Aquí puedes agregar tu código PHP para esta sección si es necesario
-// ...
-?>
